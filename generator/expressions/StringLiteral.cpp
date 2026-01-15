5df5e2ed433af73c579c69d98e00485964e7b61c
IDjinn <42049518+IDjinn@users.noreply.github.com>
2026-01-15 12:21:37 -0300

    cleanup: remove arquivos ignorados do repositório

--- generator/expressions/StringLiteral.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Luke on 06/12/2025.
-//
-
-#include "../Generator.h"
-
-llvm::Value *Generator::generate_string_literal(const StringLiteral &expr) const {
-    return builder->CreateGlobalString(expr.value);
-}
